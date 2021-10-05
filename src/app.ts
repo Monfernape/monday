aa540ca7c4a2759b3d47b3422adaffe28b3a035f
Monfernape <engr.usmankhalil@gmail.com>
2021-10-05 20:58:38 +0500

    Changing Run Command

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,23 @@
-import express, { Application, Request, Response } from 'express'
+import express, { Application, Request, Response, NextFunction } from "express";
+import cors from "cors";
 
-const app: Application = express()
+const application: Application = express();
+application.use(express.json());
+application.use(cors())
+application.use((req: Request, res: Response, next: NextFunction) => {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "*");
+      res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+      next();
+    })
 
-const port: number = 3001
+const port = 3001;
 
-app.get('/toto', (req: Request, res: Response) => {
-    res.send('Hello toto')
-})
+application.post("/monday", (req: Request, res: Response) => {
+  console.log("Body", req.body);
+  res.status(201).send(req.body);
+});
 
-app.listen(port, function () {
-    console.log(`App is listening on port ${port} !`)
-})
+application.listen(port, function () {
+  console.log(`App is listening on port ${port} !`);
+});
