24d03facd9f2cec391cfaec882d4ae40514fea37
Monfernape <engr.usmankhalil@gmail.com>
2021-10-06 18:59:03 +0500

    Added logic to get item details on status change

--- src/app.ts
+++ src/app.ts
@@ -14,8 +14,21 @@ application.use((req: Request, res: Response, next: NextFunction) => {
 const port = process.env.PORT || 3001;
 
 application.post("/status-change", (req: Request, res: Response) => {
-  console.log("Body", req.body);
+  console.log("Body", req.body.event);
+  fetch ("https://api.monday.com/v2", {
+  method: 'post',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization' : 'YOUR_API_KEY_HERE'
+   },
+   body: JSON.stringify({
+     query : `query { assets (ids: [${req.body.event.pulseId}]) { id name url }}`
+   })
+  }).then(response => {
+    console.log("Item Response", response)
+  }).catch(error => console.log('Item Error', error));
   res.status(200).send(req.body);
+
 });
 
 application.listen(port, function () {
