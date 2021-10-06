77f4470623354c09c596d8de618427a4f629d29f
Monfernape <engr.usmankhalil@gmail.com>
2021-10-06 20:18:16 +0500

    Fixed node-fetch import

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express, { Application, Request, Response, NextFunction } from "express";
-import fetch from "node-fetch";
+import nodeFetch from "node-fetch";
 import cors from "cors";
 
 const application: Application = express();
@@ -16,23 +16,24 @@ const port = process.env.PORT || 3001;
 
 application.post("/status-change", (req: Request, res: Response) => {
   console.log("Body", req.body.event);
-  fetch ("https://api.monday.com/v2", {
-  method: 'post',
-  headers: {
-    'Content-Type': 'application/json',
-    'Authorization' : 'YOUR_API_KEY_HERE'
-   },
-   body: JSON.stringify({
-     query : `query { assets (ids: [${req.body.event.pulseId}]) { id name url }}`
-   })
-  }).then(response => {
-    console.log("Item Response", response)
-    res.status(200).send(req.body);
-  }).catch(error => {
-    console.log('Item Error', error)
-    res.status(500).send(error);
-  });
-
+  nodeFetch("https://api.monday.com/v2", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "YOUR_API_KEY_HERE",
+    },
+    body: JSON.stringify({
+      query: `query { assets (ids: [${req.body.event.pulseId}]) { id name url }}`,
+    }),
+  })
+    .then((response) => {
+      console.log("Item Response", response);
+      res.status(200).send(req.body);
+    })
+    .catch((error) => {
+      console.log("Item Error", error);
+      res.status(500).send(error);
+    });
 });
 
 application.listen(port, function () {
