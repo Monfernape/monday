98624a2712920501ff4a30d7ed318a21619281b1
Monfernape <engr.usmankhalil@gmail.com>
2021-10-06 18:00:17 +0500

    Adding push service method for item change

--- src/app.ts
+++ src/app.ts
@@ -3,19 +3,24 @@ import cors from "cors";
 
 const application: Application = express();
 application.use(express.json());
-application.use(cors())
+application.use(cors());
 application.use((req: Request, res: Response, next: NextFunction) => {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "*");
-      res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-      next();
-    })
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  next();
+});
 
 const port = process.env.PORT || 3001;
 
 application.post("/monday", (req: Request, res: Response) => {
   console.log("Body", req.body);
   res.status(201).send(req.body);
+});
+
+application.post("/item-change", (req: Request, res: Response) => {
+  console.log("Body", req.body);
+  res.status(200).send(req.body);
 });
 
 application.listen(port, function () {
