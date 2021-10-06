8a9a9005463393f555076dedd7f1658a1773ff5e
Monfernape <engr.usmankhalil@gmail.com>
2021-10-06 19:03:04 +0500

    Added async response

--- src/app.ts
+++ src/app.ts
@@ -26,8 +26,11 @@ application.post("/status-change", (req: Request, res: Response) => {
    })
   }).then(response => {
     console.log("Item Response", response)
-  }).catch(error => console.log('Item Error', error));
-  res.status(200).send(req.body);
+    res.status(200).send(req.body);
+  }).catch(error => {
+    console.log('Item Error', error)
+    res.status(500).send(error);
+  });
 
 });
 
